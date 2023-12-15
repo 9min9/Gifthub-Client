9518a6ad71e8d4eaa8a7672557388f5a24ad2a2d
9min9 <gming0226@gmail.com>
2023-12-15 15:29:19 +0900

    feat : Add Route

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,18 +1,20 @@
 import {Route, Routes} from "react-router-dom";
-import App from "../App";
-import Header from "../components/common/Header";
 import Main from "./Main";
+import Signup from "./account/Signup";
+import Login from "./account/Login";
 
 export default function Router() {
     return (
-      <Routes>
-          <Route exact path="/" element={<Main/>}></Route>
-          <Route path="/payments" element={<Main/>}></Route>
-          <Route path="/mypage" element={<Main/>}></Route>
-          <Route path="/gifticon/add" element={<Main/>}></Route>
-          <Route path="/user/my-gifticon" element={<Main/>}></Route>
-          <Route path="/carts" element={<Main/>}></Route>
-          <Route path="/payment/checkout" element={<Main/>}></Route>
-      </Routes>
+        <Routes>
+            <Route exact path="/" element={<Main />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/payments" element={<Main/>}></Route>
+            <Route path="/mypage" element={<Main/>}></Route>
+            <Route path="/gifticon/add" element={<Main/>}></Route>
+            <Route path="/user/my-gifticon" element={<Main/>}></Route>
+            <Route path="/carts" element={<Main/>}></Route>
+            <Route path="/payment/checkout" element={<Main/>}></Route>
+        </Routes>
     );
 }
