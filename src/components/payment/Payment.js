0b1282a1f98a7b4bd79bbe1ca2c4794f6579fb20
9min9 <gming0226@gmail.com>
2023-12-18 16:30:51 +0900

    fix: Fix Router.js useContext를 사용하여 로그인 여부를 확인하는 기능을 Router.js에 추가

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -1,15 +1,5 @@
 import PaymentSection from "./PaymentSection";
-import {useContext} from "react";
-import {AuthContext} from "../account/AuthContextProvider";
-import Login from "../../pages/account/Login";
 
 export default function Payment() {
-    const {isAuthenticated} = useContext(AuthContext);
-
-    if (isAuthenticated) {
-        return <PaymentSection/>;
-    } else {
-        return <Login/>;
-    }
-
+    return <PaymentSection/>;
 }
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,21 +1,32 @@
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import Main from "./Main";
 import Signup from "./account/Signup";
 import Login from "./account/Login";
 import Payment from "../components/payment/Payment";
+import {useContext} from "react";
+import {AuthContext} from "../components/account/AuthContextProvider";
 
 export default function Router() {
+    const {isAuthenticated} = useContext(AuthContext);
+
     return (
         <Routes>
             <Route exact path="/" element={<Main/>}></Route>
             <Route path="/signup" element={<Signup/>}></Route>
             <Route path="/login" element={<Login/>}></Route>
-            <Route path="/payments" element={<Payment/>}></Route>
-            <Route path="/mypage" element={<Main/>}></Route>
-            <Route path="/gifticon/add" element={<Main/>}></Route>
-            <Route path="/user/my-gifticon" element={<Main/>}></Route>
-            <Route path="/carts" element={<Main/>}></Route>
-            <Route path="/payment/checkout" element={<Main/>}></Route>
+
+            {isAuthenticated ? (
+                    <>
+                        <Route path="/payments" element={<Payment/>}/>
+                        <Route path="/mypage" element={<Main/>}/>
+                        <Route path="/gifticon/add" element={<Main/>}/>
+                        <Route path="/user/my-gifticon" element={<Main/>}/>
+                        <Route path="/carts" element={<Main/>}/>
+                        <Route path="/payment/checkout" element={<Main/>}/>
+                    </>)
+                :
+                (<Route path="/*" element={<Navigate to="/login"/>}/>)}
         </Routes>
-    );
+    )
+        ;
 }
