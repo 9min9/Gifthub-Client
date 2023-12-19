a544dc7fa84ed62ffaba699f65c2bee7c4e788ca
9min9 <gming0226@gmail.com>
2023-12-19 11:44:18 +0900

    fix: Button.js

--- src/components/ui/button/PrimaryButton.js
+++ src/components/ui/button/PrimaryButton.js
@@ -6,6 +6,6 @@ export default function PrimaryButton(props) {
     let id = target + "-btn";
 
     return (
-        <button id={id} className="w-r__link btn--e-brand-b-2" onClick={_onClick}>{innerText}</button>
+        <a id={id} className="w-r__link btn--e-brand-b-2" onClick={_onClick}>{innerText}</a>
     )
 }
--- src/components/ui/button/WhiteButton.js
+++ src/components/ui/button/WhiteButton.js
@@ -1,10 +1,10 @@
 
 
-export default function Button(props) {
+export default function WhiteButton(props) {
     const {target, innerText, _onClick} = props;
     let id = target + "-btn";
 
     return (
-        <button id= {id} className="w-r__link btn--e-transparent-platinum-b-2" onClick={_onClick}>{innerText}</button>
+        <a id= {id} className="w-r__link btn--e-transparent-platinum-b-2" onClick={_onClick}>{innerText}</a>
     )
 }
