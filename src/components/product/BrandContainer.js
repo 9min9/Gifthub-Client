4a918776abd23bf374cc7eeb75e6c61416514bdb
J Kim <jkim987891@gmail.com>
2023-12-20 15:55:11 +0900

    Feat: Product 불러오기 및 검색

--- src/components/product/BrandContainer.js
+++ src/components/product/BrandContainer.js
@@ -1,23 +1,17 @@
 import BrandComponent from "./BrandComponent";
 
-export default function BrandContainer({brandList}) {
-    console.log(brandList);
-
+export default function BrandContainer({brandList, handleBrandClick}) {
     return <div className="section__content" id="sticky-header">
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
                     <div className="filter-category-container" id="filter-category-container">
-                        <BrandComponent
-                            brand={"전체"}
-                            checked={true}
-                            // brandClick={selectBrand}
-                        />
                         {brandList.map(brand => (
                             <BrandComponent
                                 key={brand.brandName}
                                 brand={brand.brandName}
                                 checked={brand.checked}
+                                handleBrandClick={handleBrandClick}
                             />
                         ))}
                     </div>
