79d6a87be524d247ae61ec5602e030325ad2098b
J Kim <jkim987891@gmail.com>
2023-12-21 16:30:09 +0900

    Refactor: 컴포넌트 분리

---
+++ src/components/ui/form/InputWithLabel.js
@@ -0,0 +1,8 @@
+function InputWithLabel({id, placeholder, handleSearchChange, searchInput, handleSearchKeyUp}) {
+    return <>
+        <label htmlFor={id}></label>
+        <input className="input-text input-text--border-radius input-text--style-1" type="text"
+               id={id} placeholder={placeholder} value={searchInput} onChange={handleSearchChange}
+               onKeyUp={handleSearchKeyUp}/>
+    </>;
+}
