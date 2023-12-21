95b22e2751834831d58f2f536491ce2ac81b31fb
J Kim <jkim987891@gmail.com>
2023-12-21 16:29:19 +0900

    Refactor: 컴포넌트 분리

--- src/components/ui/image-card/ProductCard.js
+++ src/components/ui/image-card/ProductCard.js
@@ -38,7 +38,7 @@ function ProductCard({product, index, products, increasePage}) {
             <div className="col-lg-3 col-md-6 u-s-m-b-30 product-wrapper">
                 <div className="product-o product-o--radius product-o--hover-off u-h-100">
                     {/*    모달*/}
-                    <a className="aspect aspect--bg-grey aspect--square u-d-block">
+                    <a className="aspect aspect--bg-grey aspect--square u-d-block" onClick={handleOpenModalClick}>
                         <img className="aspect__img" src="/images/noproductimage/no-image-departmentstore.png"/>
                     </a>
                     <ImageContent
