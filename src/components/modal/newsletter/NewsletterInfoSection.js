968242ae27b19340cf0f5e8d81a3f0720fa2795f
9min9 <gming0226@gmail.com>
2023-12-22 23:17:58 +0900

    feat : Add error message in gifticon modal
    기프티콘 등록 모달에 등록 검수 상태에 따른 error message를 출력

--- src/components/modal/newsletter/NewsletterInfoSection.js
+++ src/components/modal/newsletter/NewsletterInfoSection.js
@@ -4,16 +4,86 @@ import NewsletterModalSubSubTitleWrapper from "./NewsletterModalSubSubTitleWrapp
 import GifticonAddForm from "../../gifticion/GifticonAddForm";
 
 export default function NewsletterInfoSection({item}) {
-    return(
-        <div className="col-lg-6 new-l__col-2">
-            <div className="new-l__section u-s-m-t-30">
-                <NewsletterModalHead>
-                    <NewsletterModalTitleWrapper innerText={"기프티콘 등록하기"}/>
-                    <NewsletterModalSubSubTitleWrapper innerText={"기프티콘 정보를 확인해주세요"}/>
-                </NewsletterModalHead>
 
-                <GifticonAddForm item={item}/>
+
+    console.log(item)
+
+    if (item.flagInDb === true && item.state === "WAIT_REGISTRATION") {
+        return (
+            <div className="col-lg-6 new-l__col-2">
+                <div className="new-l__section u-s-m-t-30">
+                    <NewsletterModalHead>
+                        <NewsletterModalTitleWrapper innerText={"기프티콘 등록하기"}/>
+                        <NewsletterModalSubSubTitleWrapper innerText={"기프티콘 정보를 확인해주세요"}/>
+                    </NewsletterModalHead>
+
+                    <GifticonAddForm item={item}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+
+
+    if (item.status === "NEED_APPROVAL" && item.flagInDb === false) {
+        return (
+            <div className="col-lg-6 new-l__col-2">
+                <div className="new-l__section u-s-m-t-30">
+                    <NewsletterModalHead>
+                        <NewsletterModalTitleWrapper innerText={"기프티콘 검수 요청하기"}/>
+                        <NewsletterModalSubSubTitleWrapper errorText={"확인되지 않는 정보가 존재합니다. 검수 요청이 필요합니다"}/>
+                    </NewsletterModalHead>
+
+                    <GifticonAddForm item={item}/>
+                </div>
+            </div>
+        );
+    }
+
+    if (item.flagInDb === false) {
+        return (
+            <div className="col-lg-6 new-l__col-2">
+                <div className="new-l__section u-s-m-t-30">
+                    <NewsletterModalHead>
+                        <NewsletterModalTitleWrapper innerText={"기프티콘 검수 요청하기"}/>
+                        <NewsletterModalSubSubTitleWrapper errorText={"등록되지 않은 상품입니다. 검수 요청이 필요합니다"}/>
+                    </NewsletterModalHead>
+
+                    <GifticonAddForm item={item}/>
+                </div>
+            </div>
+        );
+    }
+
+    if (item.status === "FAIL_REGISTRATION") {
+        return (
+            <div className="col-lg-6 new-l__col-2">
+                <div className="new-l__section u-s-m-t-30">
+                    <NewsletterModalHead>
+                        <NewsletterModalTitleWrapper innerText={"기프티콘 검수 요청하기"}/>
+                        <NewsletterModalSubSubTitleWrapper errorText={"검수 요청이 거절 당했습니다"}/>
+                    </NewsletterModalHead>
+
+                    <GifticonAddForm item={item}/>
+                </div>
+            </div>
+        );
+
+    }
+
+
+
+
+    // return (
+    //     <div className="col-lg-6 new-l__col-2">
+    //         <div className="new-l__section u-s-m-t-30">
+    //             <NewsletterModalHead>
+    //                 <NewsletterModalTitleWrapper innerText={"기프티콘 등록하기"}/>
+    //                 <NewsletterModalSubSubTitleWrapper innerText={"기프티콘 정보를 확인해주세요"}/>
+    //             </NewsletterModalHead>
+    //
+    //             <GifticonAddForm item={item}/>
+    //         </div>
+    //     </div>
+    // );
 }
--- src/components/modal/newsletter/NewsletterModalSubSubTitleWrapper.js
+++ src/components/modal/newsletter/NewsletterModalSubSubTitleWrapper.js
@@ -2,12 +2,13 @@
 
 export default function NewsletterModalSubSubTitleWrapper(props) {
 
-    const {innerText} = props;
+    const {innerText, errorText, errorDetail} = props;
 
     return(
         <div className="u-s-m-b-30 new-l--center">
             <p id="product-valid" className="new-l__p1">{innerText}</p>
-            <p id="product-valid-error" className="new-l__p1"></p>
+            <p id="product-valid-error" className="new-l__p1" style={{color:"red"}}>{errorText}</p>
+            <p id="product-valid-error" className="new-l__p1" style={{color:"red"}}>{errorDetail}</p>
             <input id="status-modal-input" name="status" className="news-l__input" type="hidden"/>
         </div>
     );
