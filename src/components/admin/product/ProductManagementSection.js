39fc7716b857fe3c543ac0fc7cd8a1dfd2240997
9min9 <gming0226@gmail.com>
2023-12-23 03:06:35 +0900

    design : Create table for approval storage list table

--- src/components/admin/product/ProductManagementSection.js
+++ src/components/admin/product/ProductManagementSection.js
@@ -1,33 +1,35 @@
 import DashboardTitleContainer from "../../mypage/dashboard/DashboardTitleContainer";
 import DashboardH1 from "../../ui/dashboard/DashboardH1";
 import DashboardSpan from "../../ui/dashboard/DashboardSpan";
+import GifticonItemContainer from "../../gifticion/GifticonItemContainer";
+import GifticonTableBody from "./GifticonTableBody";
 
-export default function ProductManagementSection() {
-    return(
+export default function ProductManagementSection({itemList}) {
+    return (
         <div className="dash__box dash__box--shadow dash__box--radius dash__box--bg-white u-s-m-b-30">
             <DashboardTitleContainer>
                 <DashboardH1 innerText={"상품 관리"} margin={"u-s-p-x-20 u-s-p-t-20"}></DashboardH1>
                 <DashboardSpan innerText={"상품 등록 신청 목록"} margin={"u-s-p-x-20 u-s-p-t-10 u-s-p-b-20"}></DashboardSpan>
             </DashboardTitleContainer>
 
-                <div className="dash__table-wrap gl-scroll">
-                    <table className="dash__table">
-                        <thead id="thead">
-                        <tr>
-                            <th>이름</th>
-                            <th>브랜드</th>
-                            <th>신청인</th>
-                            <th>신청일</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody id="tbody">
-                        </tbody>
-                    </table>
-                </div>
-
-
-
+            <div className="dash__table-wrap gl-scroll">
+                <table className="dash__table">
+                    <thead id="thead">
+                    <tr>
+                        <th>이름</th>
+                        <th>브랜드</th>
+                        <th>신청인</th>
+                        <th>신청일</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody id="tbody">
+                    {itemList.map((item) => (
+                        <GifticonTableBody item={item}/>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
 
 
         </div>
