9d608b0317bdcba608165a224c49618810a36a1e
SMayor <abdortus159@naver.com>
2023-12-22 11:51:35 +0900

    feat : "기프티콘 등록"
    db에 있는 상품 등록

--- src/components/gifticion/GifticonAddForm.js
+++ src/components/gifticion/GifticonAddForm.js
@@ -1,42 +1,112 @@
 import Label from "../ui/form/Label";
 import NewsletterInputWrapper from "../modal/newsletter/NewsletterInputWrapper";
+import axios from "axios";
+import useGifticonAddFormInput from "../../hooks/useGifticonAddFormInput";
 
-export default function GifticonAddForm() {
+export default function GifticonAddForm(props) {
+    const {item} = props;
+
+    const input = useGifticonAddFormInput({
+        productName : item.productName || '',
+        brandName : item.brand || '',
+        due : item.due || '',
+        barcode : item.barcode || '',
+        price : '',
+    });
+
+    console.log("item:"+item);
+    console.log("item.storageId: " +item.gifticonStorageId);
+
+    const handleGifticonAddModalClick = async () => {
+        try {
+            const formData = new FormData();
+
+            for (const [key, value] of Object.entries(input.values)){
+                formData.append(key, value);
+            }
+            formData.append('storageId', item.gifticonStorageId);
+
+            const response = await axios.post(`${process.env.REACT_APP_API_ROOT}/api/gifticon/register`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type' : 'application/json',
+                        Authorization : localStorage.getItem('token')
+                    },
+                }).then( function (response){
+                    console.log("response : " + response);
+                    console.log("response.data : "+ response.data);
+            })
+        } catch (error){
+            console.log(error);
+        }
+    }
+
     return(
         <form id="gifticon-add-form" className="new-l__form">
 
-            <NewsletterInputWrapper labelText={"상품 이름"} inputClassName={"news-l__input"} name={"productName"} type={"text"} placeholder={"상품 이름을 입력해주세요"}></NewsletterInputWrapper>
+            <NewsletterInputWrapper
+                labelText={"상품 이름"}
+                inputClassName={"news-l__input"}
+                name={"productName"} type={"text"}
+                                    // placeholder={"상품 이름을 입력해주세요"}
+                placeholder={item.productName}
+                _onChange={(e) => input.onChange('productName', e.target.value)}
+            />
 
             <div id="error-product-div" className="u-s-m-b-10">
                 <Label labelText={""}></Label>
                 <label id="error-product-modal-label" className="gl-label"></label>
             </div>
-            <NewsletterInputWrapper labelText={"브랜드 이름"} inputClassName={"news-l__input"} name={"brandName"} type={"text"} placeholder={"브랜드 이름을 입력해주세요"}></NewsletterInputWrapper>
+            <NewsletterInputWrapper
+                labelText={"브랜드 이름"}
+                inputClassName={"news-l__input"}
+                name={"brandName"}
+                type={"text"}
+                placeholder={item.brand}
+                _onChange={(e) => input.onChange('brand', e.target.value)}
+            />
 
             <div id="error-brand-div" className="u-s-m-b-10">
                 <label id="error-brand-modal-label" className="gl-label"></label>
             </div>
 
-            <NewsletterInputWrapper labelText={"유효 기간"} inputClassName={"news-l__input"} name={"due"} type={"date"} placeholder={"유효기간을 선택해주세요"}></NewsletterInputWrapper>
+            <NewsletterInputWrapper labelText={"유효 기간"} inputClassName={"news-l__input"} name={"due"} type={"date"}
+                                    placeholder={item.due ? item.due : ""}
+            />
 
             <div id="error-due-div" className="u-s-m-b-10">
                 <label id="error-due-modal-label" className="gl-label"></label>
             </div>
 
-            <NewsletterInputWrapper labelText={"바코드 번호"} inputClassName={"news-l__input"} name={"barcode"} type={"number"} placeholder={"바코드 번호를 입력해주세요"}></NewsletterInputWrapper>
+            <NewsletterInputWrapper
+                labelText={"바코드 번호"}
+                inputClassName={"news-l__input"}
+                name={"barcode"}
+                type={"number"}
+                placeholder={item.barcode}
+                _onChange={(e) => input.onChange(`barcode`, e.target.value)}
+            />
 
             <div id="error-barcode-div" className="u-s-m-b-10">
                 <label id="error-barcode-modal-label" className="gl-label"></label>
             </div>
 
-            <NewsletterInputWrapper labelText={"구매 가격"} inputClassName={"news-l__input"} name={"price"} type={"number"} placeholder={"구매 가격을 입력해주세요"}></NewsletterInputWrapper>
+            <NewsletterInputWrapper
+                labelText={"구매 가격"}
+                inputClassName={"news-l__input"}
+                name={"price"}
+                type={"number"}
+                placeholder={item.price}
+                _onChange={(e) => input.onChange(`price`, e.target.value)}
+            />
 
             <div id="error-price-div" className="u-s-m-b-10">
                 <label id="error-price-modal-label" className="gl-label"></label>
             </div>
 
             <div className="u-s-m-y-15">
-                <a id="gifticon-add-modal-btn" className="btn btn--e-brand-b-2">등록</a>
+                <a id="gifticon-add-modal-btn" className="btn btn--e-brand-b-2" onClick={handleGifticonAddModalClick}>등록</a>
             </div>
         </form>
     );
--- src/components/modal/newsletter/NewsletterModalImageSection.js
+++ src/components/modal/newsletter/NewsletterModalImageSection.js
@@ -1,10 +1,11 @@
 import Image from "../../ui/Image";
 
-export default function NewsletterModalImageSection() {
+export default function NewsletterModalImageSection(props) {
     return(
         <div className="col-lg-6 new-l__col-1 u-s-p-x-0">
             <div className="new-l__img-wrap u-d-block">
-                <Image id={"gifticon-modal-img"} className={"u-img-fluid u-d-block"}></Image>
+                <Image id={"gifticon-modal-img"} className={"u-img-fluid u-d-block"}
+                src={props.item.imageUrl}></Image>
             </div>
         </div>
     );
