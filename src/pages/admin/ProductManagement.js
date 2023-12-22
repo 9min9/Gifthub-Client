44c2520afe63fceca78de8a4302dd1c7d8efc794
9min9 <gming0226@gmail.com>
2023-12-23 03:05:42 +0900

    feat : Get approval storage list in admin page

--- src/pages/admin/ProductManagement.js
+++ src/pages/admin/ProductManagement.js
@@ -1,7 +1,21 @@
 import ProductManagementSection from "../../components/admin/product/ProductManagementSection";
 import AdminSidebar from "../../components/admin/AdminSidebar";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function ProductManagement() {
+    const [storageList, setStorageList] = useState([]);
+
+    useEffect(() => {
+        axios
+            .post(`${process.env.REACT_APP_SERVER_URL}/api/admin/gifticon/confirm/list`, null ,{headers: {Authorization: localStorage.getItem("token")}})
+            .then(function (response) {
+                console.log(response.data.content);
+                setStorageList(response.data.content);
+            })
+
+    }, []);
+
     return (
         <div className="app-content">
             <div className="u-s-p-y-60">
@@ -15,7 +29,7 @@ export default function ProductManagement() {
                                 </div>
 
                                 <div className="col-lg-9 col-md-12">
-                                    <ProductManagementSection></ProductManagementSection>
+                                    <ProductManagementSection itemList={storageList}></ProductManagementSection>
                                 </div>
                             </div>
                         </div>
