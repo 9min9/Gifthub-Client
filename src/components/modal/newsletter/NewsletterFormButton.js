0fb7ed6d5c4c3f01959ce6363c353141fb85d858
9min9 <gming0226@gmail.com>
2023-12-23 08:04:26 +0900

    design : Set justify content in NewsletterFormButton.js.js

--- src/components/modal/newsletter/NewsletterFormButton.js
+++ src/components/modal/newsletter/NewsletterFormButton.js
@@ -5,9 +5,12 @@ export default function NewsletterFormButton(props) {
     const {innerText, _onClick} = props
 
     return(
-        <div className="u-s-m-y-15">
-            <a id="gifticon-add-modal-btn" className="btn btn--e-brand-b-2" onClick={_onClick}>{innerText}</a>
+        <div style={{display:"flex", justifyContent: "end"}}>
+            <div className="u-s-m-y-15" >
+                <a id="gifticon-add-modal-btn" className="btn btn--e-brand-b-2" onClick={_onClick}>{innerText}</a>
+            </div>
         </div>
+
     );
 
 }
