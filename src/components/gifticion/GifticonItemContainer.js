c5cd64598b838406e6918f8a5cbe7801b4d8cd66
9min9 <gming0226@gmail.com>
2023-12-23 15:57:30 +0900

    refactor : Render code by status

--- src/components/gifticion/GifticonItemContainer.js
+++ src/components/gifticion/GifticonItemContainer.js
@@ -9,9 +9,9 @@ import axios from "axios";
 import {Navigate, useNavigate} from "react-router-dom";
 
 export default function GifticonItemContainer({item}) {
-
     const { isOpen, openModal, closeModal} = useNewsletterModal();
     let navigate = useNavigate();
+    let screen;
 
     const renderErrorInfo = (content, error) => {
         return (
@@ -31,30 +31,128 @@ export default function GifticonItemContainer({item}) {
             });
     }
 
-    return (
-        <div className="w-r u-s-m-b-30">
-            <div className="w-r__container">
-                <div id="gifticon-info-wrap" className="w-r__wrap-1">
-                    <GifticonItemImage url={item.imageUrl}/>
+    const renderWaitRegistrationState = () => {
+        return (
+            <div className="w-r u-s-m-b-30">
+                <div className="w-r__container">
+                    <div id="gifticon-info-wrap" className="w-r__wrap-1">
+                        <GifticonItemImage url={item.imageUrl}/>
 
-                    <div id="gifticon-info" className="w-r__info">
-                        {renderErrorInfo(item.brand, "브랜드 이름이 존재하지 않습니다")}
-                        {renderErrorInfo(item.productName, "상품명이 존재하지 않습니다")}
-                        {renderErrorInfo(item.barcode, "바코드 번호가 존재하지 않습니다")}
-                        {renderErrorInfo(item.due, "유효기간이 존재하지 않습니다")}
-                        <GifticonHiddenInfo content={item.status}></GifticonHiddenInfo>
-                        <GifticonHiddenInfo content={item.flagInDb}></GifticonHiddenInfo>
+                        <div id="gifticon-info" className="w-r__info">
+                            {renderErrorInfo(item.brand, "브랜드 이름이 존재하지 않습니다")}
+                            {renderErrorInfo(item.productName, "상품명이 존재하지 않습니다")}
+                            {renderErrorInfo(item.barcode, "바코드 번호가 존재하지 않습니다")}
+                            {renderErrorInfo(item.due, "유효기간이 존재하지 않습니다")}
+                            <GifticonHiddenInfo content={item.status}></GifticonHiddenInfo>
+                            <GifticonHiddenInfo content={item.flagInDb}></GifticonHiddenInfo>
+                        </div>
+                    </div>
+
+                    <div id="gifticon-btn-wrap" className="w-r__wrap-2">
+                        <WhiteButton innerText="삭제" _onClick={deleteStorage}/>
+                        <PrimaryButton innerText="등록 하기" _onClick={() => openModal()}/>
+                    </div>
+
+                    <NewsletterModal item={item} isOpen={isOpen} closeModal={closeModal}/>
+                </div>
+            </div>
+        );
+    }
+
+    const renderNeedApprovalState = () => {
+        return (
+            <div className="w-r u-s-m-b-30">
+                <div className="w-r__container">
+                    <div id="gifticon-info-wrap" className="w-r__wrap-1">
+                        <GifticonItemImage url={item.imageUrl}/>
+
+                        <div id="gifticon-info" className="w-r__info">
+                            {renderErrorInfo(item.brand, "브랜드 이름이 존재하지 않습니다")}
+                            {renderErrorInfo(item.productName, "상품명이 존재하지 않습니다")}
+                            {renderErrorInfo(item.barcode, "바코드 번호가 존재하지 않습니다")}
+                            {renderErrorInfo(item.due, "유효기간이 존재하지 않습니다")}
+                            <GifticonHiddenInfo content={item.status}></GifticonHiddenInfo>
+                            <GifticonHiddenInfo content={item.flagInDb}></GifticonHiddenInfo>
+                        </div>
+                    </div>
+
+                    <div id="gifticon-btn-wrap" className="w-r__wrap-2">
+                        <WhiteButton innerText="삭제" _onClick={deleteStorage}/>
+                        <PrimaryButton innerText="등록 하기" _onClick={() => openModal()}/>
+                    </div>
+
+                    <NewsletterModal item={item} isOpen={isOpen} closeModal={closeModal}/>
+                </div>
+            </div>
+        );
+    }
+
+    const renderAdminApprovalState = () => {
+        return(
+            <div className="w-r u-s-m-b-30">
+                <div className="w-r__container">
+                    <div id="gifticon-info-wrap" className="w-r__wrap-1">
+                        <GifticonItemImage url={item.imageUrl}/>
+
+                        <div id="gifticon-info" className="w-r__info">
+                            {renderErrorInfo(item.brand, "브랜드 이름이 존재하지 않습니다")}
+                            {renderErrorInfo(item.productName, "상품명이 존재하지 않습니다")}
+                            {renderErrorInfo(item.barcode, "바코드 번호가 존재하지 않습니다")}
+                            {renderErrorInfo(item.due, "유효기간이 존재하지 않습니다")}
+                            <GifticonHiddenInfo content={item.status}></GifticonHiddenInfo>
+                            <GifticonHiddenInfo content={item.flagInDb}></GifticonHiddenInfo>
+                        </div>
+                    </div>
+
+                    <div id="gifticon-btn-wrap" className="w-r__wrap-2">
+                        <span>상품 검수 진행중 입니다.</span>
+                        <WhiteButton innerText="삭제" _onClick={deleteStorage}/>
                     </div>
                 </div>
-
-                <div id="gifticon-btn-wrap" className="w-r__wrap-2">
-                    <WhiteButton innerText="삭제" _onClick={deleteStorage}/>
-                    <PrimaryButton innerText="등록 하기" _onClick={() => openModal()}/>
-                </div>
-
-                <NewsletterModal item={item} isOpen={isOpen} closeModal={closeModal}/>
             </div>
-        </div>
 
+        )
+    }
+
+    const renderFailRegistrationState = () => {
+        return (
+            <div className="w-r u-s-m-b-30">
+                <div className="w-r__container">
+                    <div id="gifticon-info-wrap" className="w-r__wrap-1">
+                        <GifticonItemImage url={item.imageUrl}/>
+
+                        <div id="gifticon-info" className="w-r__info">
+                            {renderErrorInfo(item.brand, "브랜드 이름이 존재하지 않습니다")}
+                            {renderErrorInfo(item.productName, "상품명이 존재하지 않습니다")}
+                            {renderErrorInfo(item.barcode, "바코드 번호가 존재하지 않습니다")}
+                            {renderErrorInfo(item.due, "유효기간이 존재하지 않습니다")}
+                            <GifticonHiddenInfo content={item.status}></GifticonHiddenInfo>
+                            <GifticonHiddenInfo content={item.flagInDb}></GifticonHiddenInfo>
+                        </div>
+                    </div>
+
+                    <div id="gifticon-btn-wrap" className="w-r__wrap-2">
+                        <WhiteButton innerText="삭제" _onClick={deleteStorage}/>
+                        <PrimaryButton innerText="등록 하기" _onClick={() => openModal()}/>
+                    </div>
+
+                    <NewsletterModal item={item} isOpen={isOpen} closeModal={closeModal}/>
+                </div>
+            </div>
+        );
+    }
+
+    if (item.status === "WAIT_REGISTRATION") {
+        screen = renderWaitRegistrationState();
+    } else if (item.status === "NEED_APPROVAL") {
+        screen = renderNeedApprovalState();
+    } else if(item.status === "ADMIN_APPROVAL") {
+        screen = renderAdminApprovalState();
+    } else if(item.status === "FAIL_REGISTARTION"){
+        screen = renderFailRegistrationState();
+    }
+
+    return (
+        screen
     )
 }
