7b6899181ff4e0d0e46f81647597ae9697d1b6ad
9min9 <gming0226@gmail.com>
2023-12-23 15:35:32 +0900

    refactor : Render code by status

--- src/components/modal/newsletter/NewsletterInfoSection.js
+++ src/components/modal/newsletter/NewsletterInfoSection.js
@@ -5,9 +5,24 @@ import GifticonAddForm from "../../gifticion/GifticonAddForm";
 
 export default function NewsletterInfoSection({item}) {
     let buttonText;
-    console.log(item)
+    let screen;
 
-    if (item.flagInDb === true) {
+    const renderAdminScreen = () => {
+        buttonText = "검수 확인";
+        return (
+            <div className="col-lg-6 new-l__col-2">
+                <div className="new-l__section u-s-m-t-30">
+                    <NewsletterModalHead>
+                        <NewsletterModalTitleWrapper innerText={"기프티콘 검수 확인하기"}/>
+                    </NewsletterModalHead>
+
+                    <GifticonAddForm item={item} buttonText={buttonText}/>
+                </div>
+            </div>
+        );
+    }
+
+    const renderRegistrationScreen = () => {
         buttonText="등록"
         return (
             <div className="col-lg-6 new-l__col-2">
@@ -23,42 +38,8 @@ export default function NewsletterInfoSection({item}) {
         );
     }
 
-    if (item.flagInDb === true && item.state === "WAIT_REGISTRATION") {
-        buttonText="등록"
-        return (
-            <div className="col-lg-6 new-l__col-2">
-                <div className="new-l__section u-s-m-t-30">
-                    <NewsletterModalHead>
-                        <NewsletterModalTitleWrapper innerText={"기프티콘 등록하기"}/>
-                        <NewsletterModalSubSubTitleWrapper innerText={"기프티콘 정보를 확인해주세요"}/>
-                    </NewsletterModalHead>
-
-                    <GifticonAddForm item={item} buttonText={buttonText}/>
-                </div>
-            </div>
-        );
-    }
-
-    if (item.status === "NEED_APPROVAL" && item.flagInDb === false) {
-        buttonText="등록 검수"
-        // setButtonText("등록 검수");
-        return (
-            <div className="col-lg-6 new-l__col-2">
-                <div className="new-l__section u-s-m-t-30">
-                    <NewsletterModalHead>
-                        <NewsletterModalTitleWrapper innerText={"기프티콘 검수 요청하기"}/>
-                        <NewsletterModalSubSubTitleWrapper errorText={"확인되지 않는 정보가 존재합니다. 검수 요청이 필요합니다"}/>
-                    </NewsletterModalHead>
-
-                    <GifticonAddForm item={item} buttonText={buttonText}/>
-                </div>
-            </div>
-        );
-    }
-
-    if (item.flagInDb === false) {
+    const renderApprovalScreen = () => {
         buttonText = "등록 검수";
-        // setButtonText("등록 검수");
         return (
             <div className="col-lg-6 new-l__col-2">
                 <div className="new-l__section u-s-m-t-30">
@@ -73,30 +54,14 @@ export default function NewsletterInfoSection({item}) {
         );
     }
 
-    if (item.status === "FAIL_REGISTRATION") {
+    const renderFailRegistrationScreen = () => {
         buttonText = "등록 검수";
         return (
             <div className="col-lg-6 new-l__col-2">
                 <div className="new-l__section u-s-m-t-30">
                     <NewsletterModalHead>
                         <NewsletterModalTitleWrapper innerText={"기프티콘 검수 요청하기"}/>
-                        <NewsletterModalSubSubTitleWrapper errorText={"검수 요청이 거절 당했습니다"}/>
-                    </NewsletterModalHead>
-
-                    <GifticonAddForm item={item} buttonText={buttonText} />
-                </div>
-            </div>
-        );
-    }
-
-
-    if(item.isAdmin === true) {
-        buttonText = "검수 확인";
-        return (
-            <div className="col-lg-6 new-l__col-2">
-                <div className="new-l__section u-s-m-t-30">
-                    <NewsletterModalHead>
-                        <NewsletterModalTitleWrapper innerText={"기프티콘 검수 확인하기"}/>
+                        <NewsletterModalSubSubTitleWrapper errorText={"등록 검수가 취소되었습니다"} errorDetail={"정보를 다시 확인 해주세요"}/>
                     </NewsletterModalHead>
 
                     <GifticonAddForm item={item} buttonText={buttonText}/>
@@ -104,4 +69,16 @@ export default function NewsletterInfoSection({item}) {
             </div>
         );
     }
+
+    if(item.isAdmin === true) {
+        screen = renderAdminScreen();
+    } else if(item.flagInDb === true && item.status === "WAIT_REGISTRATION") {
+        screen =renderRegistrationScreen();
+    } else if (item.status === "NEED_APPROVAL"){
+        screen = renderApprovalScreen();
+    } else if (item.status === "FAIL_REGISTRATION") {
+        screen = renderFailRegistrationScreen();
+    }
+
+    return(screen);
 }
