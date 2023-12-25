7390676dbfaae4fed561bbaa4f85e3d87b0f3992
9min9 <gming0226@gmail.com>
2023-12-25 17:31:32 +0900

    fix : Change api url to login button

--- src/components/ui/button/KakaoLoginButton.js
+++ src/components/ui/button/KakaoLoginButton.js
@@ -1,11 +1,9 @@
-
-
 export default function KakaoLoginButton(props) {
     const {target, innerText} = props;
 
     let id = target + "-btn";
-    let redirectUrl = "http://15.164.69.187:8080/login";
-    let href ="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=578c006810acb509f2ccc52277d13ec9&redirect_uri="+ redirectUrl+"&prompt=select_account"
+    let redirectUrl = `${process.env.REACT_APP_SERVER_HOME_URL}/login`;
+    let href = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=578c006810acb509f2ccc52277d13ec9&redirect_uri=" + redirectUrl + "&prompt=select_account"
 
     return (
         <button id={id} className="btn--icon" type="button">
--- src/components/ui/button/NaverLoginButton.js
+++ src/components/ui/button/NaverLoginButton.js
@@ -1,10 +1,9 @@
-
 export default function NaverLoginButton(props) {
     const {target, innerText} = props;
 
     let id = target + "-btn";
-    let redirectUrl = "http://localhost:3000/login";
-    let href ="https://nid.naver.com/oauth2.0/authorize?client_id=iw7Eqm8dtb9NpQ22vmxe&redirect_uri="+ redirectUrl+"&response_type=code"
+    let redirectUrl = `${process.env.REACT_APP_SERVER_HOME_URL}/login`;
+    let href = "https://nid.naver.com/oauth2.0/authorize?client_id=iw7Eqm8dtb9NpQ22vmxe&redirect_uri=" + redirectUrl + "&response_type=code"
 
     return (
         <button className="btn--icon" type="button">
